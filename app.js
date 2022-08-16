57775cceda47e7d99d8680911947254cb1549d9d
AdrianoDSousa <89171060+AdrianoDSousa@users.noreply.github.com>
2022-08-15 21:31:45 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let nomeDeUsuario = "Adriano";
+    console.log(nomeDeUsuario);
+let sobreNome = "de Sousa";
+    console.log(sobreNome);
+
+function CC (nomeDeUsuario, sobreNome) {
+   CC = nomeDeUsuario ; sobreNome;
+}
+console.log(CC);
---
+++ ex3.js
@@ -0,0 +1,9 @@
+var agua = 6;
+var picole = 2;
+
+function bomDia(){
+    return"Ola! Bom dia!";
+};
+
+bomDia = bomDia
+console.log(bomDia);
